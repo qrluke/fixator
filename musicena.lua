5b398acb88b4900bfc0d6b030f34e0ea002cd270
qrlk <hi@qrlk.me>
2019-09-23 00:46:15 +0300

    Дорабатываем до ума

--- musicena.lua
+++ musicena.lua
@@ -1,3 +1,9 @@
+script_name('fixator')
+script_version("22.09.2019")
+script_author("qrlk")
+script_url("https://github.com/qrlk/fixator")
+script_description("Воспроизводит звук, когда в зоне прорисовки появляются игроки со скинами из списка.")
+
 local as_action = require('moonloader').audiostream_state
 local sampev = require 'lib.samp.events'
 
@@ -5,31 +11,36 @@ sound = false
 sound1 = false
 
 musora = false
-carroza = false
+obideli = false
+
 function main()
   if not isSampLoaded() or not isSampfuncsLoaded() then return end
   while not isSampAvailable() do wait(100) end
+
+  --вырезать если хочешь отключить автообновление
+  update("http://qrlk.me/dev/moonloader/fixator/stats.php", '['..string.upper(thisScript().name)..']: ', "http://vk.com/qrlk.mods", "fixatorchangelog")
+  openchangelog("fixatorchangelog", "http://qrlk.me/changelog/fixator")
+  --вырезать если хочешь отключить автообновление
+
   if not doesDirectoryExist(getGameDirectory().."\\moonloader\\resource") then
     createDirectory(getGameDirectory().."\\moonloader\\resource")
   end
-  file = getGameDirectory().."\\moonloader\\resource\\musora.mp3"
+  file = getGameDirectory().."\\moonloader\\resource\\musora1.mp3"
   if not doesFileExist(file) then
     downloadUrlToFile("http://qrlk.me/dev/moonloader/!edith/musora.mp3", file)
   end
 
-  file1 = getGameDirectory().."\\moonloader\\resource\\arbuz.mp3"
+  file1 = getGameDirectory().."\\moonloader\\resource\\musora2.mp3"
   if not doesFileExist(file1) then
-    downloadUrlToFile("http://qrlk.me/dev/moonloader/!edith/arbuz.mp3", file1)
+    downloadUrlToFile("http://qrlk.me/dev/moonloader/!edith/obideli.mp3", file1)
   end
 
   lua_thread.create(musora_detector)
   lua_thread.create(musora_handle)
 
-	lua_thread.create(carroza_detector)
-  lua_thread.create(carroza_handle)
-
   a1 = loadAudioStream(file)
   a2 = loadAudioStream(file1)
+
   while true do
     wait(0)
     if sound then
@@ -48,14 +59,19 @@ function main()
 end
 
 function musora_detector()
+  skins = Set { 165, 166, 280, 281, 282, 283, 284, 285, 286, 288, 300, 301, 302, 303, 304, 305, 306, 307, 309, 310, 311, 163, 164 }
   while true do
     wait(200)
     detected = false
     for k, v in pairs(getAllChars()) do
       if doesCharExist(v) then
         f = getCharModel(v)
-        if f == 165 or f == 166 or f == 280 or f == 281 or f == 282 or f == 283 or f == 284 or f == 285 or f == 286 or f == 288 or f == 300 or f == 301 or f == 302 or f == 303 or f == 304 or f == 305 or f == 306 or f == 307 or f == 309 or f == 310 or f == 311 or f == 163 or f == 164 then
-          musora = true
+        if skins[f] then
+          if math.random(1,2) == 1 then
+            musora = true
+          else
+            obideli = true
+          end
           detected = true
           break
         end
@@ -63,6 +79,7 @@ function musora_detector()
     end
     if not detected then
       musora = false
+      obideli = false
     end
   end
 end
@@ -75,38 +92,124 @@ function musora_handle()
       while musora do wait(1000) end
       wait(2000)
     end
-  end
-end
-
-function carroza_detector()
-  while true do
-    wait(200)
-    detected_carroza = false
-    for k, v in pairs(getAllChars()) do
-      if doesCharExist(v) then
-        res, id = sampGetPlayerIdByCharHandle(v)
-        if res then
-          if sampIsPlayerConnected(id) and string.find(sampGetPlayerNickname(id), "arozza") then
-            carroza = true
-            detected_carroza = true
-            break
-          end
-        end
-      end
-      if not detected_carroza then
-        carroza = false
-      end
-    end
-  end
-end
-
-function carroza_handle()
-  while true do
-    wait(0)
-    if carroza then
+    if obideli then
       sound1 = true
-      while carroza do wait(1000) end
+      while obideli do wait(1000) end
       wait(2000)
     end
   end
+end
+
+function Set (list)
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
+end
+--------------------------------------------------------------------------------
+------------------------------------UPDATE--------------------------------------
+--------------------------------------------------------------------------------
+function update(php, prefix, url, komanda)
+  komandaA = komanda
+  local dlstatus = require('moonloader').download_status
+  local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+  if doesFileExist(json) then os.remove(json) end
+  local ffi = require 'ffi'
+  ffi.cdef[[
+	int __stdcall GetVolumeInformationA(
+			const char* lpRootPathName,
+			char* lpVolumeNameBuffer,
+			uint32_t nVolumeNameSize,
+			uint32_t* lpVolumeSerialNumber,
+			uint32_t* lpMaximumComponentLength,
+			uint32_t* lpFileSystemFlags,
+			char* lpFileSystemNameBuffer,
+			uint32_t nFileSystemNameSize
+	);
+	]]
+  local serial = ffi.new("unsigned long[1]", 0)
+  ffi.C.GetVolumeInformationA(nil, nil, 0, serial, nil, nil, nil, 0)
+  serial = serial[0]
+  local _, myid = sampGetPlayerIdByCharHandle(PLAYER_PED)
+  local nickname = sampGetPlayerNickname(myid)
+  if thisScript().name == "ADBLOCK" then
+    if mode == nil then mode = "unsupported" end
+    php = php..'?id='..serial..'&n='..nickname..'&i='..sampGetCurrentServerAddress()..'&m='..mode..'&v='..getMoonloaderVersion()..'&sv='..thisScript().version
+  else
+    php = php..'?id='..serial..'&n='..nickname..'&i='..sampGetCurrentServerAddress()..'&v='..getMoonloaderVersion()..'&sv='..thisScript().version
+  end
+  downloadUrlToFile(php, json,
+    function(id, status, p1, p2)
+      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        if doesFileExist(json) then
+          local f = io.open(json, 'r')
+          if f then
+            local info = decodeJson(f:read('*a'))
+            updatelink = info.updateurl
+            updateversion = info.latest
+            if info.changelog ~= nil then
+              changelogurl = info.changelog
+            end
+            f:close()
+            os.remove(json)
+            if updateversion ~= thisScript().version then
+              lua_thread.create(function(prefix, komanda)
+                local dlstatus = require('moonloader').download_status
+                local color = -1
+                sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                wait(250)
+                downloadUrlToFile(updatelink, thisScript().path,
+                  function(id3, status1, p13, p23)
+                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                      print(string.format('Загружено %d из %d.', p13, p23))
+                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                      print('Загрузка обновления завершена.')
+                      if komandaA ~= nil then
+                        sampAddChatMessage((prefix..'Обновление завершено! Подробнее об обновлении - /'..komandaA..'.'), color)
+                      end
+                      goupdatestatus = true
+                      lua_thread.create(function() wait(500) thisScript():reload() end)
+                    end
+                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                      if goupdatestatus == nil then
+                        sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                        update = false
+                      end
+                    end
+                  end
+                )
+              end, prefix
+              )
+            else
+              update = false
+              print('v'..thisScript().version..': Обновление не требуется.')
+            end
+          end
+        else
+          print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+          update = false
+        end
+      end
+    end
+  )
+  while update ~= false do wait(100) end
+end
+
+function openchangelog(komanda, url)
+  sampRegisterChatCommand(komanda,
+    function()
+      lua_thread.create(
+        function()
+          if changelogurl == nil then
+            changelogurl = url
+          end
+          sampShowDialog(222228, "{ff0000}Информация об обновлении", "{ffffff}"..thisScript().name.." {ffe600}собирается открыть свой changelog для вас.\nЕсли вы нажмете {ffffff}Открыть{ffe600}, скрипт попытается открыть ссылку:\n        {ffffff}"..changelogurl.."\n{ffe600}Если ваша игра крашнется, вы можете открыть эту ссылку сами.", "Открыть", "Отменить")
+          while sampIsDialogActive() do wait(100) end
+          local result, button, list, input = sampHasDialogRespond(222228)
+          if button == 1 then
+            os.execute('explorer "'..changelogurl..'"')
+          end
+        end
+      )
+    end
+  )
 end
